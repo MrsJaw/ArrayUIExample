6c9940c98d2d04ec839739df28ce8f8d2ef9915a
Julia Winegeart <bryson.julia@gmail.com>
2016-06-21 10:43:47 -0500

    Added .cs files via upload
    Added Program.cs, the form and its designer.

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Infragistics.Win.Appearance appearance1 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance2 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance3 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance4 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance5 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance6 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance7 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance8 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance9 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance10 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance11 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance12 = new Infragistics.Win.Appearance();
+            this.groupBoxDataGrid = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBoxUltraGrid = new System.Windows.Forms.GroupBox();
+            this.ultraGrid1 = new Infragistics.Win.UltraWinGrid.UltraGrid();
+            this.groupBoxListBox = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBoxFlowLayoutPanel = new System.Windows.Forms.GroupBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.groupBoxDataGrid.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBoxUltraGrid.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ultraGrid1)).BeginInit();
+            this.groupBoxListBox.SuspendLayout();
+            this.groupBoxFlowLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxDataGrid
+            // 
+            this.groupBoxDataGrid.Controls.Add(this.dataGridView1);
+            this.groupBoxDataGrid.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBoxDataGrid.Location = new System.Drawing.Point(0, 0);
+            this.groupBoxDataGrid.Name = "groupBoxDataGrid";
+            this.groupBoxDataGrid.Size = new System.Drawing.Size(377, 181);
+            this.groupBoxDataGrid.TabIndex = 0;
+            this.groupBoxDataGrid.TabStop = false;
+            this.groupBoxDataGrid.Text = "Data Grid View";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 16);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(371, 162);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBoxUltraGrid
+            // 
+            this.groupBoxUltraGrid.Controls.Add(this.ultraGrid1);
+            this.groupBoxUltraGrid.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBoxUltraGrid.Location = new System.Drawing.Point(0, 181);
+            this.groupBoxUltraGrid.Name = "groupBoxUltraGrid";
+            this.groupBoxUltraGrid.Size = new System.Drawing.Size(377, 228);
+            this.groupBoxUltraGrid.TabIndex = 1;
+            this.groupBoxUltraGrid.TabStop = false;
+            this.groupBoxUltraGrid.Text = "Ultra Grid";
+            // 
+            // ultraGrid1
+            // 
+            appearance1.BackColor = System.Drawing.SystemColors.Window;
+            appearance1.BorderColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ultraGrid1.DisplayLayout.Appearance = appearance1;
+            this.ultraGrid1.DisplayLayout.BorderStyle = Infragistics.Win.UIElementBorderStyle.Solid;
+            this.ultraGrid1.DisplayLayout.CaptionVisible = Infragistics.Win.DefaultableBoolean.False;
+            appearance2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            appearance2.BackColor2 = System.Drawing.SystemColors.ControlDark;
+            appearance2.BackGradientStyle = Infragistics.Win.GradientStyle.Vertical;
+            appearance2.BorderColor = System.Drawing.SystemColors.Window;
+            this.ultraGrid1.DisplayLayout.GroupByBox.Appearance = appearance2;
+            appearance3.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.ultraGrid1.DisplayLayout.GroupByBox.BandLabelAppearance = appearance3;
+            this.ultraGrid1.DisplayLayout.GroupByBox.BorderStyle = Infragistics.Win.UIElementBorderStyle.Solid;
+            appearance4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            appearance4.BackColor2 = System.Drawing.SystemColors.Control;
+            appearance4.BackGradientStyle = Infragistics.Win.GradientStyle.Horizontal;
+            appearance4.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.ultraGrid1.DisplayLayout.GroupByBox.PromptAppearance = appearance4;
+            this.ultraGrid1.DisplayLayout.MaxColScrollRegions = 1;
+            this.ultraGrid1.DisplayLayout.MaxRowScrollRegions = 1;
+            appearance5.BackColor = System.Drawing.SystemColors.Window;
+            appearance5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.ultraGrid1.DisplayLayout.Override.ActiveCellAppearance = appearance5;
+            appearance6.BackColor = System.Drawing.SystemColors.Highlight;
+            appearance6.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.ultraGrid1.DisplayLayout.Override.ActiveRowAppearance = appearance6;
+            this.ultraGrid1.DisplayLayout.Override.BorderStyleCell = Infragistics.Win.UIElementBorderStyle.Dotted;
+            this.ultraGrid1.DisplayLayout.Override.BorderStyleRow = Infragistics.Win.UIElementBorderStyle.Dotted;
+            appearance7.BackColor = System.Drawing.SystemColors.Window;
+            this.ultraGrid1.DisplayLayout.Override.CardAreaAppearance = appearance7;
+            appearance8.BorderColor = System.Drawing.Color.Silver;
+            appearance8.TextTrimming = Infragistics.Win.TextTrimming.EllipsisCharacter;
+            this.ultraGrid1.DisplayLayout.Override.CellAppearance = appearance8;
+            this.ultraGrid1.DisplayLayout.Override.CellClickAction = Infragistics.Win.UltraWinGrid.CellClickAction.EditAndSelectText;
+            this.ultraGrid1.DisplayLayout.Override.CellPadding = 0;
+            appearance9.BackColor = System.Drawing.SystemColors.Control;
+            appearance9.BackColor2 = System.Drawing.SystemColors.ControlDark;
+            appearance9.BackGradientAlignment = Infragistics.Win.GradientAlignment.Element;
+            appearance9.BackGradientStyle = Infragistics.Win.GradientStyle.Horizontal;
+            appearance9.BorderColor = System.Drawing.SystemColors.Window;
+            this.ultraGrid1.DisplayLayout.Override.GroupByRowAppearance = appearance9;
+            appearance10.TextHAlignAsString = "Left";
+            this.ultraGrid1.DisplayLayout.Override.HeaderAppearance = appearance10;
+            this.ultraGrid1.DisplayLayout.Override.HeaderClickAction = Infragistics.Win.UltraWinGrid.HeaderClickAction.SortMulti;
+            this.ultraGrid1.DisplayLayout.Override.HeaderStyle = Infragistics.Win.HeaderStyle.WindowsXPCommand;
+            appearance11.BackColor = System.Drawing.SystemColors.Window;
+            appearance11.BorderColor = System.Drawing.Color.Silver;
+            this.ultraGrid1.DisplayLayout.Override.RowAppearance = appearance11;
+            this.ultraGrid1.DisplayLayout.Override.RowSelectors = Infragistics.Win.DefaultableBoolean.False;
+            appearance12.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ultraGrid1.DisplayLayout.Override.TemplateAddRowAppearance = appearance12;
+            this.ultraGrid1.DisplayLayout.ScrollBounds = Infragistics.Win.UltraWinGrid.ScrollBounds.ScrollToFill;
+            this.ultraGrid1.DisplayLayout.ScrollStyle = Infragistics.Win.UltraWinGrid.ScrollStyle.Immediate;
+            this.ultraGrid1.DisplayLayout.ViewStyleBand = Infragistics.Win.UltraWinGrid.ViewStyleBand.OutlookGroupBy;
+            this.ultraGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ultraGrid1.Location = new System.Drawing.Point(3, 16);
+            this.ultraGrid1.Name = "ultraGrid1";
+            this.ultraGrid1.Size = new System.Drawing.Size(371, 209);
+            this.ultraGrid1.TabIndex = 0;
+            this.ultraGrid1.Text = "ultraGrid1";
+            // 
+            // groupBoxListBox
+            // 
+            this.groupBoxListBox.Controls.Add(this.listBox1);
+            this.groupBoxListBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBoxListBox.Location = new System.Drawing.Point(0, 409);
+            this.groupBoxListBox.Name = "groupBoxListBox";
+            this.groupBoxListBox.Size = new System.Drawing.Size(377, 117);
+            this.groupBoxListBox.TabIndex = 2;
+            this.groupBoxListBox.TabStop = false;
+            this.groupBoxListBox.Text = "List Box";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 16);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(371, 98);
+            this.listBox1.TabIndex = 0;
+            // 
+            // groupBoxFlowLayoutPanel
+            // 
+            this.groupBoxFlowLayoutPanel.Controls.Add(this.flowLayoutPanel1);
+            this.groupBoxFlowLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxFlowLayoutPanel.Location = new System.Drawing.Point(0, 526);
+            this.groupBoxFlowLayoutPanel.Name = "groupBoxFlowLayoutPanel";
+            this.groupBoxFlowLayoutPanel.Size = new System.Drawing.Size(377, 155);
+            this.groupBoxFlowLayoutPanel.TabIndex = 3;
+            this.groupBoxFlowLayoutPanel.TabStop = false;
+            this.groupBoxFlowLayoutPanel.Text = "Flow Layout Panel";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 16);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(371, 136);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 681);
+            this.Controls.Add(this.groupBoxFlowLayoutPanel);
+            this.Controls.Add(this.groupBoxListBox);
+            this.Controls.Add(this.groupBoxUltraGrid);
+            this.Controls.Add(this.groupBoxDataGrid);
+            this.Name = "Form1";
+            this.Text = "Array UI Examples";
+            this.groupBoxDataGrid.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBoxUltraGrid.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ultraGrid1)).EndInit();
+            this.groupBoxListBox.ResumeLayout(false);
+            this.groupBoxFlowLayoutPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxDataGrid;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBoxUltraGrid;
+        private Infragistics.Win.UltraWinGrid.UltraGrid ultraGrid1;
+        private System.Windows.Forms.GroupBox groupBoxListBox;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBoxFlowLayoutPanel;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+
+        #region Constructors...
+
+        #region Form1     
+        public Form1()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+        #endregion Form1 
+
+        #endregion Constructors...
+
+        #region Methods...
+        
+        #region Initialize     
+        private void Initialize()
+        {
+            TeamMember[] Data = InitializeData();
+
+            //Data Grid View
+            dataGridView1.DataSource = Data;
+
+            //Ultra Grid
+            ultraGrid1.DataSource = Data;
+
+            //List Box
+            Dictionary<string, TeamMember> TeamMemberByName = new Dictionary<string, TeamMember>();
+            foreach(TeamMember member in Data)
+            {
+                string FullName = string.Format("{0} {1}. {2}", member.FirstName, member.MiddleInitial, member.LastName);
+                TeamMemberByName.Add(FullName, member);
+            }
+            listBox1.DataSource = TeamMemberByName.Keys.ToArray();
+            listBox1.MultiColumn = true;
+            listBox1.SelectionMode = SelectionMode.MultiExtended;
+
+            //Flow Layout Panel
+            foreach(TeamMember member in Data)
+            {
+                CheckBox TeamMemberCheckBox = new CheckBox();
+                TeamMemberCheckBox.Text = string.Format("{0} {1}. {2}", member.FirstName, member.MiddleInitial, member.LastName);
+                TeamMemberCheckBox.Tag = member;
+                TeamMemberCheckBox.AutoSize = true;
+
+                flowLayoutPanel1.Controls.Add(TeamMemberCheckBox);
+            }
+        }
+        #endregion Initialize
+        
+        #region InitializeData     
+        private TeamMember[] InitializeData()
+        {
+            List<TeamMember> Result = new List<TeamMember>();
+            Result.Add(new TeamMember("Julia", "B", "Winegeart"));
+            Result.Add(new TeamMember("Adam", "N", "Poell"));
+            Result.Add(new TeamMember("Greg", "A", "Grieshaber"));
+            Result.Add(new TeamMember("Yogesh", "K", "Tamrakar"));
+            return Result.ToArray();
+        }
+        #endregion InitializeData
+
+        #endregion Methods...
+
+    }
+
+    #region TeamMember
+    public class TeamMember
+    {
+        public bool Select { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleInitial { get; set; }
+        public string LastName { get; set; }
+
+        public TeamMember()
+        {
+            FirstName = "John";
+            MiddleInitial = "Q";
+            LastName = "Public";
+        }
+
+        public TeamMember(string firstname, string middleinitial, string lastname)
+        {
+            FirstName = firstname;
+            MiddleInitial = middleinitial;
+            LastName = lastname;
+        }
+    }
+    #endregion TeamMember
+}
